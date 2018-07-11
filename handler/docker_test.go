c3da4c98da1da0f052ea38b288129954a0194ad4
caijiaqi <caijiaqi2017@gmail.com>
2018-07-11 22:03:12 +0800

    add test code for run code api

---
+++ handler/docker_test.go
@@ -0,0 +1,70 @@
+package handle
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunCpp(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+	router.POST("/:language", RunCode)
+	w := httptest.NewRecorder()
+
+	buf := bytes.NewBufferString(cppHelloWorld)
+	req, _ := http.NewRequest("POST", "/cpp", buf)
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"userResult":{"stdout":"Hello, World! cpp","stderr":"","exiterror":""},"taskError":""}`, w.Body.String())
+}
+
+func TestRunGo(t *testing.T) {
+	router := gin.New()
+	router.POST("/:language", RunCode)
+
+	w := httptest.NewRecorder()
+
+	buf := bytes.NewBufferString(goHelloWorld)
+	req, _ := http.NewRequest("POST", "/go", buf)
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"userResult":{"stdout":"Hello, World! go\n","stderr":"","exiterror":""},"taskError":""}`, w.Body.String())
+}
+
+const cppHelloWorld = `
+{
+	"files":[
+		{
+			"content":"#include <iostream>\n\nint main()\n{\n    std::cout << \"Hello, World! cpp\";\n}",
+			"name":"main.cpp"
+		}
+	],
+	"stdin":"",
+	"argument":{
+		"compile":[],
+		"run":[]
+	}
+}
+`
+const goHelloWorld = `
+{
+	"files" : [
+		{
+		"name":"hi.go",
+		"content":"package main\n\nimport \"fmt\"\nfunc main() {\n    fmt.Println(\"Hello, World! go\")\n}"
+		}
+	],
+	"language" : "go",
+	"argument" : {
+		"compile":[],
+		"run" :[]
+	}
+ }
+`
